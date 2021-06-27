80eebe1280ca86e559313127e91d7e189026e3e2
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-06-27 12:56:29 +0530

    Added the javascript but not working...not adding input to the listItems

---
+++ todo.js
@@ -0,0 +1,44 @@
+var enterButton= document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementById("li");
+
+function createListElement(){
+    var li=document.createElement("li");
+    li.appendChild(document.createTextNode(input));
+    ul.appendChild(li);
+    input.value=" ";
+    function crossOut(){
+        li.classList.toggle("done");
+
+    }
+    li.addEventListener("click",crossOut);
+
+    function deleteListItem(){
+        li.classList.add("delete");
+    }
+
+    var dBtn = document.createElement("button")
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click",deleteListItem);
+}
+
+function inputLength(){
+    return input.value.length;
+}
+
+function addListAfterClick(){
+    if(inputLength()>0){
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(){
+    if(inputLength()>0 && event.which===13){
+        createListElement();
+    }
+}
+enterButton.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress",addListAfterKeypress);
